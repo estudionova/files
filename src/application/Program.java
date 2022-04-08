f62181c079a2051f2c978044a807eec3a4fb2a07
Reinaldo Junior <42872129+estudionova@users.noreply.github.com>
2022-04-08 10:12:37 -0300

    FileReader e BufferedReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,55 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
-        File file = new File("C:\\temp\\in.txt");
+        String path = "c:\\temp\\in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /* File file = new File("C:\\temp\\in.txt");
         Scanner sc = null;
 
         try {
@@ -26,6 +68,6 @@ public class Program {
                 sc.close();
             }
         }
-
+*/
     }
 }
